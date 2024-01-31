1a6682aa3677a27bb19c4533bf3b98a82861c942
P3ANTMC <pean.tmc@seznam.cz>
2024-01-31 22:56:48 +0100

    Done

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        String nameAndSurname = "František Cibule";
+        LocalDate birthdate = LocalDate.of(1985, 2, 14);
+        int numberOfAgreements = 32;
+        double carrotSold = 237.6; // in tons
+        String residenceCity = "Jablunkov";
+        String licencePlate = "8T3 2343";
+        double companyVehicleConsumption = 9.2; // l/100km
+        String companyCompIP = "192.138.42.39";
+
+        BigDecimal averageQuantityPerAgreements = BigDecimal.valueOf(carrotSold / numberOfAgreements);
+        System.out.println(
+                "The average quantity of sold carrots per contract is " + averageQuantityPerAgreements + " tons.");
     }
 }
